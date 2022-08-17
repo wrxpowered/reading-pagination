aeb77fd037e7779ede69bbc9416e993db72d2b49
WRX <wrxpowered@gmail.com>
2022-08-17 14:23:54 +0800

    migrate popper.js to floating-ui

--- src/annotation.js
+++ src/annotation.js
@@ -1,8 +1,17 @@
-import { createPopper } from './popper';
-import { isElement, createHtmlString, encodeHtmlString } from './utilities';
+import {
+  computePosition, 
+  offset, 
+  shift, 
+  flip, 
+  arrow
+} from './floating-ui/dom';
+import { 
+  isElement, 
+  createHtmlString, 
+  encodeHtmlString 
+} from './utilities';
 
 
-const ARROW_SIZE = 8;
 let _popper = null;
 let _popperEle = null;
 
@@ -33,20 +42,36 @@ function showAnnotation (ele) {
   );
   document.body.insertAdjacentElement('beforeend', _popperEle);
 
-  _popper = createPopper(ele, _popperEle, {
-    placement: 'auto',
-    modifiers: [
-      {
-        name: 'eventListeners',
-        enabled: false,
-      },
-      {
-        name: 'offset',
-        options: {
-          offset: [0, ARROW_SIZE],
-        },
-      },
+  const arrowElement = _popperEle.querySelector('.annotation-arrow')
+  computePosition(ele, _popperEle, {
+    placement: 'bottom',
+    middleware: [
+      offset(6),
+      flip(),
+      shift({padding: 5}),
+      arrow({element: arrowElement})
     ],
+  }).then(({x, y, placement, middlewareData}) => {
+    Object.assign(_popperEle.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    });
+
+    // handle arrow
+    const {x: arrowX, y: arrowY} = middlewareData.arrow;
+    const staticSide = {
+      top: 'bottom',
+      right: 'left',
+      bottom: 'top',
+      left: 'right',
+    }[placement.split('-')[0]];
+    Object.assign(arrowElement.style, {
+      left: arrowX != null ? `${arrowX}px` : '',
+      top: arrowY != null ? `${arrowY}px` : '',
+      right: '',
+      bottom: '',
+      [staticSide]: '-4px',
+    });
   });
 }
 
